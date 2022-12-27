830fdb2907855120e1e5c236e286e801076992cf
ahmedtawfikali <108682970+ahmedtawfikali@users.noreply.github.com>
2022-12-27 14:24:59 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,28 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+function mFunction() {
+var x = document.getElementById("myDIV");
+if (x.style.display === "none") {
+x.style.display = "block";
+} else {
+x.style.display = "none";
+}
+}
+
+function aFunction() {
+var x = document.getElementById("myDIVv");
+if (x.style.display === "none") {
+x.style.display = "block";
+} else {
+x.style.display = "none";
+}
+}
+
+
